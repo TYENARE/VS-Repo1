2c7cea4d729be7428f7a1f115a0a854239ef9f23
Tejashree <tejashree.yenare@x-scape.se>
2021-03-24 16:50:17 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS_Repo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
