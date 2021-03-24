ade42dfbea39dd1cd3d59a0222eeb87de3a2d77a
Tejashree <tejashree.yenare@x-scape.se>
2021-03-24 16:53:58 +0100

    added new line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace VS_Repo1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New Changes!");
+
         }
     }
 }
